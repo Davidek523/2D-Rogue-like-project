a1717dabec90d239a6f725422a2340403f4be749
Davidek523 <davidov520@wp.pl>
2025-10-21 13:16:25 +0200

    Added teleportation mechanic
    Added:
    - the imp can teleport from his spawn to a random position on the screen with a cooldown

--- Imp.cs
+++ Imp.cs
@@ -3,6 +3,9 @@ using System.Numerics;
 class Imp : Enemy
 {
     public Fireball FireballWeapon;
+    private Random rand = new Random();
+    public float TeleportationCooldown = 0f;
+    public float MaxTeleportationCooldown = 6f;
     public override int Attack { get; set; } = 15;
     public Imp(int width, int height, float x, float y) : base(width, height, x, y)
     {
@@ -13,9 +16,25 @@ class Imp : Enemy
     {
         base.Update(player, deltaTime);
 
+        Teleport(deltaTime);
+
         FireballWeapon.Update(player, this, deltaTime);
         FireballWeapon.Attack(this, player);
         MapCollision(X, Y);
+    }
+
+    public void Teleport(float deltaTime)
+    {
+        if (TeleportationCooldown <= 0)
+        {
+            X = rand.Next(0, Raylib_cs.Raylib.GetScreenWidth() - Width);
+            Y = rand.Next(0, Raylib_cs.Raylib.GetScreenHeight() - Height);
+            TeleportationCooldown = MaxTeleportationCooldown;
+        }
+        else
+        {
+            TeleportationCooldown -= 1f * deltaTime;
+        }
     }
 
     public override void Draw()
